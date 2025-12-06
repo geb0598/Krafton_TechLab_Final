6e6f90fa66ef1fdc8d3be8f7bca45fc65b815758
geb0598 <geb0598@gmail.com>
2025-12-06 22:21:15 +0900

    fix: 김민찬 애니메이션 오류 수정

--- Mundi/Source/Runtime/Engine/GameFramework/Vehicle.cpp
+++ Mundi/Source/Runtime/Engine/GameFramework/Vehicle.cpp
@@ -1,12 +1,21 @@
 ﻿#include "pch.h"
 #include "Vehicle.h"
+
+#include "AnimStateMachine.h"
+#include "AnimStateMachineInstance.h"
 #include "InputComponent.h"
+#include "SkeletalMeshComponent.h"
 
 AVehicle::AVehicle()
     : CurrentForwardInput(0.0f)
     , CurrentSteeringInput(0.0f)
     , CurrentTorqueInput(0.0f)
     , bIsBoosting(false)
+    , bLeanLeftInput(false)
+    , bLeanRightInput(false)
+    , Driver(nullptr)
+    , DriverStateMachine(nullptr)
+    , StateId_Idle(-1)
 {
     ChassisMesh = CreateDefaultSubobject<UStaticMeshComponent>("ChassisMesh");
     FString ChassisFileName = GDataDir + "/Model/ShoppingCart/ShoppingCart.obj";
@@ -43,13 +52,18 @@ AVehicle::AVehicle()
 
     SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
     SpringArm->SetupAttachment(ChassisMesh);
-    SpringArm->TargetArmLength = 10.0f;
+    SpringArm->TargetArmLength = 20.0f;
     SpringArm->SocketOffset = FVector(-4.6f, 0.0f, 4.6f);
     SpringArm->bUsePawnControlRotation = true;
 
     Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
     Camera->SetupAttachment(SpringArm);
     Camera->SetFarClipPlane(1000.f);
+
+    Driver = CreateDefaultSubobject<USkeletalMeshComponent>("Driver");
+    Driver->SetupAttachment(ChassisMesh);
+    Driver->SetRelativeLocation(FVector(-1.43f, 0.0f, 0.27f));
+    Driver->SetSkeletalMesh(GDataDir + "/Brian/Brian.fbx");
 }
 
 AVehicle::~AVehicle()
@@ -59,6 +73,37 @@ AVehicle::~AVehicle()
 void AVehicle::BeginPlay()
 {
     Super::BeginPlay();
+
+    if (Driver)
+    {
+        Driver->UseStateMachine();
+        DriverStateMachine = Driver->GetOrCreateStateMachine();
+
+        if (DriverStateMachine)
+        {
+            auto& ResMgr = UResourceManager::GetInstance();
+            auto* AnimIdle = ResMgr.Get<UAnimSequence>(GDataDir + "/Brian/Brian_Armature_Idle");
+            auto* AnimLeft = ResMgr.Get<UAnimSequence>(GDataDir + "/Brian/Brian_Armature_Lean_Left");
+            auto* AnimRight = ResMgr.Get<UAnimSequence>(GDataDir + "/Brian/Brian_Armature_Lean_Right");
+
+            FAnimState StateIdle;
+            StateIdle.Name = "Idle";
+            StateIdle.bLooping = true;
+            StateId_Idle = DriverStateMachine->AddState(StateIdle, AnimIdle);
+
+            FAnimState StateLeft;
+            StateLeft.Name = "LeanLeft";
+            StateLeft.bLooping = false; // 1프레임 포즈 유지
+            StateId_LeanLeft = DriverStateMachine->AddState(StateLeft, AnimLeft);
+
+            FAnimState StateRight;
+            StateRight.Name = "LeanRight";
+            StateRight.bLooping = false; // 1프레임 포즈 유지
+            StateId_LeanRight = DriverStateMachine->AddState(StateRight, AnimRight);
+
+            DriverStateMachine->SetCurrentState(StateId_Idle, 0.0f);
+        }
+    }
 }
 
 void AVehicle::Tick(float DeltaSeconds)
@@ -115,6 +160,8 @@ void AVehicle::Tick(float DeltaSeconds)
     }
 
     SyncWheelVisuals();
+
+    UpdateDriverAnimation(DeltaSeconds);
 }
 
 void AVehicle::SetupPlayerInputComponent(UInputComponent* InInputComponent)
@@ -141,6 +188,9 @@ void AVehicle::SetupPlayerInputComponent(UInputComponent* InInputComponent)
     // A 키: Scale -1.0f -> 우로 기울어짐
     InInputComponent->BindAxis<AVehicle>("AddTorque_E", 'E', -1.0f, this, &AVehicle::AddTorque);
     
+    InInputComponent->BindAction<AVehicle>("LeanLeft",  'Q', this, &AVehicle::LeanLeftPressed,  &AVehicle::LeanLeftReleased);
+    InInputComponent->BindAction<AVehicle>("LeanRight", 'E', this, &AVehicle::LeanRightPressed, &AVehicle::LeanRightReleased);
+    
     // [액션 바인딩] Space로 핸드브레이크
     // VK_SPACE는 0x20
     InInputComponent->BindAction<AVehicle>("Handbrake", VK_SPACE, this, &AVehicle::HandbrakePressed, &AVehicle::HandbrakeReleased);
@@ -163,6 +213,51 @@ void AVehicle::MoveRight(float Val)
 void AVehicle::AddTorque(float Val)
 {
     CurrentTorqueInput += Val;
+}
+
+void AVehicle::LeanLeftPressed()  { bLeanLeftInput = true; }
+void AVehicle::LeanLeftReleased() { bLeanLeftInput = false; }
+void AVehicle::LeanRightPressed()  { bLeanRightInput = true; }
+void AVehicle::LeanRightReleased() { bLeanRightInput = false; }
+
+void AVehicle::UpdateDriverAnimation(float DeltaSeconds)
+{
+    if (!DriverStateMachine) return;
+
+    // 현재 상태 인덱스
+    int32 CurrentStateIdx = DriverStateMachine->GetCurrentStateIndex();
+    
+    // 목표 상태 및 블렌딩 시간 결정
+    int32 DesiredStateIdx = StateId_Idle;
+    float BlendTime = 0.2f;
+
+    // 1. 입력에 따른 목표 상태 설정
+    if (bLeanLeftInput)
+    {
+        DesiredStateIdx = StateId_LeanLeft;
+        BlendTime = 0.15f; // [Fast In] 빠르게 기울임
+    }
+    else if (bLeanRightInput)
+    {
+        DesiredStateIdx = StateId_LeanRight;
+        BlendTime = 0.15f; // [Fast In] 빠르게 기울임
+    }
+    else
+    {
+        // 입력 없음 -> IDLE 복귀
+        DesiredStateIdx = StateId_Idle;
+
+        // [Slow Out] 기울어진 상태였다면 천천히 복귀
+        if (CurrentStateIdx == StateId_LeanLeft || CurrentStateIdx == StateId_LeanRight)
+        {
+            BlendTime = 0.5f; // 천천히 무게중심 회복
+        }
+    }
+
+    if (CurrentStateIdx != DesiredStateIdx)
+    {
+        DriverStateMachine->SetCurrentState(DesiredStateIdx, BlendTime);
+    }
 }
 
 void AVehicle::HandbrakePressed()
