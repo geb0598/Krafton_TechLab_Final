ff53bd5f9f540709f08f1c1139b236a341f1bb2c
Jinhyeok Choi <90018372+haedam19@users.noreply.github.com>
2025-12-08 00:08:36 +0900

    chore: 씬 작업

--- Mundi/Data/Scripts/test.lua
+++ Mundi/Data/Scripts/test.lua
@@ -1,8 +1,14 @@
+--[[
+시작 지점 : (166.79, 1.3, 3)
+농구장 통과 후 : (86.0, -108.0, 3)
+대로 통과 후 : (71.3, -243.7, 3)
+]]
+
 local GameMode = nil
 function BeginPlay()
     print("[BeginPlay] " .. Obj.UUID)
     GameMode = GetOwnerAs(Obj, "AGameModeBase")
-    GameMode.PlayerSpawnLocation = Vector(166.79, 1.3, 3)
+    GameMode.PlayerSpawnLocation = Vector(71.3, -233.7, 3)
     GameMode.PlayerSpawnRotationEuler = Vector(0, 0, 180.0)
 end
 
